75d22334e4c358b75bb6f1accc7abbf30f22c3c6
Oieu <nunezaoieu@gmail.com>
2024-12-11 15:51:30 +0800

    Formatted the Home Recycler View and added placeholders, still subject to change, if DB will be integrated

--- Final_Project/app/src/main/java/com/example/final_project/MainActivity.java
+++ Final_Project/app/src/main/java/com/example/final_project/MainActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +21,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        List<Tasks> tasksList = new ArrayList<Tasks>();
+
+        tasksList.add(new Tasks("Zoom Meeting", "Meeting of zoom with members", new Date()));
+        tasksList.add(new Tasks("Zoom Meeting", "Meeting of zoom with members", new Date()));
+        tasksList.add(new Tasks("Zoom Meeting", "Meeting of zoom with members", new Date()));
+        tasksList.add(new Tasks("Zoom Meeting", "Meeting of zoom with members", new Date()));
+        tasksList.add(new Tasks("Zoom Meeting", "Meeting of zoom with members", new Date()));
+        tasksList.add(new Tasks("Zoom Meeting", "Meeting of zoom with members", new Date()));
+
+        RecyclerView taskRecyclerView = findViewById(R.id.taskRecyclerView);
+        taskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskRecyclerView.setAdapter(new TasksAdapter(getApplicationContext(),tasksList));
+
+
     }
 }
